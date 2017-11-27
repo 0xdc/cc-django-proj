9aa2cc95e7776d8983dc0a35b752a57bdda4d8c8
Daniel Cordero (oak) <oak@0xdc.io>
2017-11-27 14:15:55 +0000

    mysql: allow overriding port
    Set the default to 3306

--- {{ cookiecutter.name }}/{{ cookiecutter.name }}/settings/mysql.py
+++ {{ cookiecutter.name }}/{{ cookiecutter.name }}/settings/mysql.py
@@ -1,11 +1,14 @@
 from . import *
 
+os.environ.setdefault("DB_DEFAULT_PORT", "3306")
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'HOST': env("DB_DEFAULT_HOST"),
         'NAME': env("DB_DEFAULT_NAME"),
         'USER': env("DB_DEFAULT_USER"),
+        'PORT': env("DB_DEFAULT_PORT"),
         'PASSWORD': env("DB_DEFAULT_PASSWORD"),
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
